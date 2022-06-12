5440293a96ccf7539452ee7154fa35b1381e0636
wfercanas <fernando.canas@outlook.com>
2022-06-11 23:21:35 -0500

    add/ fetch genres from api fetch

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+const API = "https://api.themoviedb.org/3";
+const GENRES_ENDPOINT = "/genre/movie/list";
+
+async function getGenres() {
+  const response = await fetch(`${API}${GENRES_ENDPOINT}?api_key=${API_KEY}`);
+  const { genres } = await response.json();
+
+  const genresList = document.querySelector("#genres ul");
+  console.log(genres);
+  console.log(genresList);
+  genres.forEach((genre) => {
+    const item = createGenre(genre.name);
+    appendToTarget(genresList, item);
+  });
+}
+
+function createGenre(name) {
+  const listItem = document.createElement("li");
+  listItem.textContent = name;
+
+  return listItem;
+}
+
+function appendToTarget(target, element) {
+  target.appendChild(element);
+}
+
+getGenres();
