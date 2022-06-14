457bb47b19f430ca05a1cd894010a153f2aa6cce
wfercanas <fernando.canas@outlook.com>
2022-06-14 16:08:54 -0500

    add/ endpoint for looking movies by genre

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,7 @@ const API = "https://api.themoviedb.org/3";
 const IMAGES_API = "https://image.tmdb.org/t/p/w500";
 const API_KEY_PARAM = `api_key=${API_KEY}`;
 const GENRES_ENDPOINT = "/genre/movie/list";
+const GENRE_MOVIES_ENDPOINT = "/discover/movie";
 
 const TRENDING_ENDPOINT = (media_type, time_window) => {
   return `/trending/${media_type}/${time_window}`;
