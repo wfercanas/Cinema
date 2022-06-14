84466d2a08199c73bff097d2f3f301efc3fe916e
wfercanas <fernando.canas@outlook.com>
2022-06-13 23:07:03 -0500

    add/ insert fetched content in movie page

--- src/movie.js
+++ src/movie.js
@@ -1,3 +1,9 @@
+const movieTitle = document.querySelector("#movie-title");
+const movieRating = document.querySelector("#movie-rating");
+const movieDescription = document.querySelector("#movie-description");
+const moviePoster = document.querySelector("#movie-poster");
+const movieGenresList = document.querySelector("#movie-genres-list");
+
 function getMovieId() {
   const search = location.search;
   const searchSplit = search.split("=");
@@ -11,6 +17,21 @@ async function getMovie() {
   const response = await fetch(`${API}${MOVIE_ENDPOINT(id)}?${API_KEY_PARAM}`);
   const data = await response.json();
   console.log(data);
+
+  movieTitle.textContent = data.original_title;
+  movieRating.textContent = data.vote_average;
+  movieDescription.textContent = data.overview;
+  moviePoster.src = `${IMAGES_API}${data.poster_path}`;
+
+  data.genres.forEach((genre) => createMovieGenre(genre.name));
+}
+
+function createMovieGenre(genre) {
+  const item = document.createElement("li");
+  item.textContent = genre;
+  item.classList.add("movie-genre");
+
+  movieGenresList.appendChild(item);
 }
 
 getMovie();
