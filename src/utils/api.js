16faa1bbd1c7746a7cde9f9bd79290236be008b5
wfercanas <fernando.canas@outlook.com>
2022-06-14 22:47:33 -0500

    add/ endpoint for querying movies

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,6 +3,7 @@ const IMAGES_API = "https://image.tmdb.org/t/p/w500";
 const API_KEY_PARAM = `api_key=${API_KEY}`;
 const GENRES_ENDPOINT = "/genre/movie/list";
 const GENRE_MOVIES_ENDPOINT = "/discover/movie";
+const SEARCH_ENDPOINT = "/search/movie";
 
 const TRENDING_ENDPOINT = (media_type, time_window) => {
   return `/trending/${media_type}/${time_window}`;
